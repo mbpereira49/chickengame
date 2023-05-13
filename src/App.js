83ab6da7d77654ca6817fd2d50badc5d6c02dcad
Matthew Pereira <mbpereira49@gmail.com>
2023-05-13 18:06:28 -0400

    adding project

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+import places from "./data";
+
+function App() {
+  const [showTile, setShowTile] = useState(['treehouse', 'wagon']);
+  const [numTiles, setNumTiles] = useState(2);
+  const [passwords, setPasswords] = useState({});
+  const [unlockedTiles, setUnlockedTiles] = useState(['treehouse'])
+
+  const handlePasswordSubmit = (id) => {
+    const index = places.findIndex((place) => place.id === id);
+    const place = places.find((place) => place.id === id);
+    const enteredPassword = passwords[places[index].id];
+    if (enteredPassword === places[index].password) {
+      /* if (!('unlocks' in places[index]) {
+        return;
+      } */
+      const unlockedIds = places[index].unlocks;
+      //const updatedUnlockedTiles = [...showTile, ...unlockedTiles, ...unlockedIds.map((id) => places.findIndex((place) => place.id === id))];
+      const updatedUnlockedTiles = [...showTile, ...unlockedTiles, ...unlockedIds];
+      const uniqueUpdatedUnlockedTiles = [... new Set(updatedUnlockedTiles)]
+      console.log(uniqueUpdatedUnlockedTiles);
+      setShowTile(uniqueUpdatedUnlockedTiles);
+  
+      if (showTile.length + 1 === numTiles && numTiles < places.length) {
+        setNumTiles(numTiles + 2);
+      }
+    }
+  };
+
+  const handlePasswordChange2 = (index, password) => {
+    const updatedPasswords = [...passwords];
+    updatedPasswords[index] = password;
+    setPasswords(updatedPasswords);
+  };
+
+  const handlePasswordChange = (id, password) => {
+    console.log(passwords);
+    setPasswords((prevPasswords) => ({ ...prevPasswords, [id]: password }));
+  };
+
+  const challengeDiv = (place) => {
+    if (!('Challenge' in place)) return;
+    return <p><b>Challenge: </b>{place.Challenge}</p>;
+  };
+
+  const riddleDiv = (place) => {
+    return ('prompt' in place) && (
+      <div className="passwordInput">
+        <p><b>Question: </b>{place.prompt}</p>
+        <label htmlFor={`passwordInput-${place.id}`}>Answer: </label>
+        <input
+          type="password"
+          id={`passwordInput-${place.id}`}
+          onChange={(e) => handlePasswordChange(place.id, e.target.value)}
+        />
+        <button onClick={() => handlePasswordSubmit(place.id)}>Submit</button>
+      </div>
+    )
+  };
+
+  const renderPlaces = () => {
+    const tiles = [];    
+    for (const id of showTile) {
+      if (!places.some((place) => place.id === id)) {
+        continue;
+      }
+      const place = places.find((x) => x.id === id);
+      const isShown = showTile.includes(place.id);
+      const hasUnlocked = ('unlocks' in place) && place.unlocks.every((id) => showTile.includes(id));
+
+      tiles.push(
+        <div className="tile" key={place.id}>
+          <div className="tileTitle" onClick={() => setShowTile([...showTile, place.id])}>
+            {place.Bar}
+          </div>
+          {isShown && (
+            <div className="tileDescription">
+              {challengeDiv(place)}
+              {riddleDiv(place)}
+            </div>
+          )}
+        </div>
+      );
+    }
+    return tiles;
+  };
+  
+
+  return <div className="tilesContainer">{renderPlaces()}</div>;
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,105 @@
+const places2 = [
+    {
+      id: 0,
+      title: "Place 1",
+      password: "password1",
+      description: "Description for place 1",
+      unlocks: [2, 3]
+    },
+    {
+      id: 1,
+      title: "Place 2",
+      password: "password2",
+      description: "Description for place 2",
+      unlocks: [4, 5]
+    },
+    {
+      id: 2,
+      title: "Place 3",
+      password: "password3",
+      unlocks: [3]
+    },
+    {
+      id: 3,
+      title: "Place 4",
+      password: "password4",
+      description: "Description for place 4",
+      unlocks: [3]
+    },
+    {
+      id: 4,
+      title: "Place 5",
+      password: "password5",
+      description: "Description for place 5",
+      unlocks: [3]
+    },
+    {
+      id: 5,
+      title: "Place 6",
+      password: "password6",
+      description: "Description for place 6",
+      unlocks: [3]
+    }
+  ];
+
+  const places = [
+    {
+      id: 'bandits',
+      Bar: 'Bandits',
+      Challenge: 'Collectively do 100 squats (in the bar)'
+    },
+    {
+      id: 'sheets',
+      Bar: '3 sheets',
+      Challenge: 'Convert your rupees to at least 1 dollar'
+    },
+    {
+      id: 'peculiar',
+      Bar: 'Peculiar pub',
+      Challenge: 'Make a friend (no speaking allowed)'
+    },
+    {
+      id: 'blind',
+      Bar: 'Blind tiger',
+      Challenge: 'outside: Pretend to be a street photographer named Johnny Pace (show IG profile) and take a photo of a stranger but it’s really bad'
+    },
+    {
+      id: 'trionas',
+      Bar: 'Triona’s on Sullivan',
+      Challenge: 'Turn to someone and express how you can\'t believe you ran into each other (conversation 34 seconds minimum)'
+    },
+    {
+      id: 'treehouse',
+      Bar: 'Greenwich Treehouse',
+      Challenge: 'Convince a group of people that you’re all british',
+      prompt: 'Fill in the missing word: image',
+      password: 'yoda',
+      unlocks: ['hatch', 'villagetavern', 'halfpint', 'sheets']
+    },
+    {
+      id: 'hatch',
+      Bar: 'Down the hatch',
+      Challenge: 'go up to a group and be like “down the hatch!!!” And chug your drink (bonus points if you get them to copy you)'
+    },
+    {
+      id: 'wagon',
+      Bar: 'Off the wagon',
+      Challenge: 'Go up to someone at the bar and say IM FUTURE YOU YOU NEED to learn about the 9 waYS TO FUCK UP YOUR OATMEAL and hand them that article',
+      prompt: 'There is a map on the second story of this bar. If you were to walk from Myrtle-Wickoff Aves. to Woodside 61st, you would pass through 3 cemeteries, but only one is depicted on the map. Which one is it?',
+      password: 'mt zion',
+      unlocks: ['peculiar', 'trionas', 'bandits', 'blind']
+    },
+    {
+      id: 'villagetavern',
+      Bar: 'The village tavern',
+      Challenge: 'pass off the idol from 5 below'
+    },
+    {
+      id: 'halfpint',
+      Bar: 'The Half Pint / Ernie’s bar',
+      Challenge: 'find someone who you necklaces astrology sign and give it to them'
+    }
+  ];  
+  
+  export default places;
+  
